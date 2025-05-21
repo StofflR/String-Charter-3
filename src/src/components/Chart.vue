3ad636cfd9787f0e7543d6967bce05c53da14ae3
Stephan Robinig <stephan.robinig@student.tugraz.at>
2025-05-21 20:17:11 +0200

    cleanup
    gh page
    
    Updates

--- src/src/components/Chart.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="w-full">
-    <div id="stop-detail" class="flex justify-center mx-8"> --- hover over a stop to see details ---</div>
-    <div id="string-graph-card" class="justify-center w-full">
-      <canvas id="graphCanvas" class="justify-center mt-2 w-full overflow-y-scroll"></canvas>
-    </div>
-  </div>
-</template>
