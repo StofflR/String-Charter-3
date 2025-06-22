b827f95286838d95588bc198d7b1a73b77c2fac5
Stephan Robinig <stephan.robinig@student.tugraz.at>
2025-06-22 17:43:12 +0200

    reorder top menu
    * add views
    * pre load zip data in appInstance constructor

--- src/components/TopMenu.vue
+++ src/components/TopMenu.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import { appInstance } from '../AppSettings';
+import {ref} from 'vue';
+import {appInstance} from '../AppSettings';
 
 const fileInput = ref<HTMLInputElement | null>(null);
 const dataFiles = appInstance.dataFiles;
@@ -9,32 +9,15 @@ const triggerFileUpload = () => {
 };
 
 let activeIndex = ref(-1);
-let menus = [
-        {
-            title: 'View',
-            action: null,
-            submenu: [
-                {
-                    title: 'tbd',
-                    action: example
-                }, 
-            ]
-        },
-    ];
 
-function example() {
-    alert(123);
+function showAbout() {
+  alert("String Charter\nVersion 3.0.0\nMIT License 2025");
 }
 
 function toggleSubMenu(index: number) {
-    index = activeIndex.value == index ? -1 : index;
-    activeIndex.value = index;
+  index = activeIndex.value == index ? -1 : index;
+  activeIndex.value = index;
 }
-
-import obbData from '../assets/datasets/GTFS_OP_2025_obb.zip';
-import goNortheastData from '../assets/datasets/gonortheast_1747916855.zip'
-appInstance.fetchDataFile("ÖBB 2025", obbData);
-appInstance.fetchDataFile("GO NORTHEAST 2025", goNortheastData);
 
 function handleUploadButton(e: Event) {
   const inputElement = e.target as HTMLInputElement;
@@ -45,42 +28,49 @@ function handleUploadButton(e: Event) {
 
 
 <template>
-<input ref="fileInput" @change="handleUploadButton" type="file" id="file-input" accept=".zip" class="hidden"/>
-<nav class="cursor-pointer bg-gray-100">
+  <input ref="fileInput" @change="handleUploadButton" type="file" id="file-input" accept=".zip" class="hidden"/>
+  <nav class="cursor-pointer bg-gray-100">
     <ul class="flex">
-        <li>
-            <div @click="toggleSubMenu(0)" class="px-1 hover:bg-gray-300 " :class="{'bg-gray-300': activeIndex == 0}">
-            File
-            </div>
-            <ul class="absolute z-1 bg-gray-300 rounded-b" v-show="activeIndex == 0">
-            <li class="px-1 hover:bg-gray-400" @click="triggerFileUpload">
-                Open...
-            </li>
-            <li v-for="dataFile in dataFiles" class="px-1 hover:bg-gray-400" @click="appInstance.handleGtfsUpload(dataFile.data)">
-                Load {{dataFile.name}}
-            </li>
-            </ul>
-        </li>
-        <li>
-            <div @click="toggleSubMenu(1)" class="px-1 hover:bg-gray-300 " :class="{'bg-gray-300': activeIndex == 1}">
-            Export
-            </div>
-            <ul class="absolute z-1 bg-gray-300 rounded-b" v-show="activeIndex == 1">
-            <li class="px-1 hover:bg-gray-400" @click="appInstance.svgGen?.exportAsSVG">
-                Export as SVG
-            </li>
-            </ul>
-        </li>
-        <li v-for="(menu, index) in menus" :key="index" @click="menu.action">
-            <div @click="toggleSubMenu(index+2)" class="px-1 hover:bg-gray-300 " :class="{'bg-gray-300': activeIndex == index+2}">
-                {{ menu.title }}
-            </div>
-            <ul class="absolute z-1 bg-gray-300 rounded-b" v-show="activeIndex == index+2">
-                <li class="px-1 hover:bg-gray-400" v-for="(sub, subIndex) in menu.submenu" :key="subIndex" @click="sub.action">
-                    {{ sub.title }}
-                </li>
-            </ul>
-        </li>
+      <li>
+        <div @click="toggleSubMenu(0)" class="px-1 hover:bg-gray-300 " :class="{'bg-gray-300': activeIndex == 0}">
+          File
+        </div>
+        <ul class="absolute z-1 bg-gray-300 rounded-b" v-show="activeIndex == 0">
+          <li class="px-1 hover:bg-gray-400" @click="triggerFileUpload">
+            Open...
+          </li>
+          <li v-for="dataFile in dataFiles" class="px-1 hover:bg-gray-400"
+              @click="appInstance.handleGtfsUpload(dataFile.data)">
+            Load {{ dataFile.name }}
+          </li>
+        </ul>
+      </li>
+      <li>
+        <div @click="toggleSubMenu(1)" class="px-1 hover:bg-gray-300 " :class="{'bg-gray-300': activeIndex == 1}">
+          View
+        </div>
+        <ul class="absolute z-1 bg-gray-300 rounded-b" v-show="activeIndex == 1">
+          <li class="px-1 hover:bg-gray-400" v-for="(sub, subIndex) in appInstance.sideMenuComponents" :key="subIndex"
+              @click="appInstance.swapViewComponent(sub.component)">
+            {{ sub.component }}
+          </li>
+        </ul>
+      </li>
+      <li>
+        <div @click="toggleSubMenu(2)" class="px-1 hover:bg-gray-300 " :class="{'bg-gray-300': activeIndex == 2 }">
+          Export
+        </div>
+        <ul class="absolute z-1 bg-gray-300 rounded-b" v-show="activeIndex == 2">
+          <li class="px-1 hover:bg-gray-400" @click="appInstance.svgGen?.exportAsSVG">
+            Export as SVG
+          </li>
+        </ul>
+      </li>
+      <li>
+        <div @click="showAbout()" class="px-1 hover:bg-gray-300 " :class="{'bg-gray-300': activeIndex == 3}">
+          About
+        </div>
+      </li>
     </ul>
-</nav>
+  </nav>
 </template>
